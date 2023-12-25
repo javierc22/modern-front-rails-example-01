45cceec344fbb23f223fe157664258694017b6eb
javierc22 <javier.ec.22@gmail.com>
2023-12-25 17:14:35 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,5 +8,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  resources :favorites
+  resource :schedule
+  resources :shopping_carts
+  resources :ticket_orders
+  resources :tickets
+  resources :gigs
+  resources :concerts
+  resources :bands
+  resources :venues
+  devise_for :users
   root to: "schedules#show"
 end
