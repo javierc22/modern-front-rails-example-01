a5c8868f02c2b3d36ff81b7887ab6c88ca08bd79
javierc22 <javier.ec.22@gmail.com>
2023-12-26 21:30:44 -0300

    Extending Our Page with Turbo Streams

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
+    "./app/views/**/*.turbostream.erb",
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
