c0ec08d5b6156a35d7234af6ed25d1e107ecb0a7
javierc22 <javier.ec.22@gmail.com>
2024-01-07 14:21:16 -0300

    Animating CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   content: [
-    "./app/views/**/*.html.erb",
-    "./app/views/**/*.turbostream.erb",
+    "./app/views/**/*.(html|turbostream).erb",
     "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/javascript/**/*.tsx",
-    "./app/javascript/**/*.ts",
+    "./app/javascript/**/*.(js|ts|tsx)",
     "./config/initializers/simple_form_tailwind.rb",
   ],
 }
